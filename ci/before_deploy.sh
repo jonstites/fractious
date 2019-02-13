c380151108f7890a971b005abeab24240e8d4eea
Jonathan Stites <mail@jonstites.com>
2019-02-13 07:47:00 -0600

    Updates ci and performs release testing

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,33 +1,18 @@
-# This script takes care of building your crate and packaging it for release
-
-set -ex
+#!/bin/bash
 
 main() {
-    local src=$(pwd) \
-          stage=
 
-    case $TRAVIS_OS_NAME in
-        linux)
-            stage=$(mktemp -d)
-            ;;
-        osx)
-            stage=$(mktemp -d -t tmp)
-            ;;
-    esac
+    cargo build --target "$TARGET" --release
+    
+    local tmpdir="$(mktemp -d)"
+    local name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
+    local staging="${tmpdir}/${name}"
 
-    test -f Cargo.lock || cargo generate-lockfile
+    cp target/$TARGET/release/fractious $staging/
+    cp {README.md, LICENSE-MIT, LICENSE-APACHE} $staging
 
-    # Update this to build the artifacts that matter to you
-    cross rustc --target $TARGET --release -- -C lto
-
-    # Update this to package the right artifacts
-    cp target/$TARGET/release/fractious $stage/
-
-    cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    cd $src
-
-    rm -rf $stage
+    (cd "$tmpdir" && tar czf "$(pwd)/deployment/${name}.tar.gz" "${name}")
+    rm -rf $tmpdir
 }
 
 main
