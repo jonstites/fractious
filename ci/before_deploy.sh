3ed5f86e34cb16395c08c7e38daaf9fc1bc525db
Jonathan Stites <mail@jonstites.com>
2019-02-13 20:46:46 -0600

    Fixes directory issues in ci

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -7,12 +7,15 @@ main() {
     local tmpdir="$(mktemp -d)"
     local name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
     local staging="${tmpdir}/${name}"
-    
+    local out_dir="$(pwd)/deployment"
+
+    mkdir -p out_dir
     mkdir -p ${staging}
+    
     cp target/$TARGET/release/fractious $staging/
     cp {README.md,LICENSE-MIT,LICENSE-APACHE} $staging
 
-    (cd "$tmpdir" && tar czf "$(pwd)/deployment/${name}.tar.gz" "${name}")
+    (cd "$tmpdir" && tar czf "$out_dir/${name}.tar.gz" "${name}")
     rm -rf $tmpdir
     set +ex
 }
