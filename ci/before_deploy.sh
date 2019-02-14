db6a484e8c7d65bfb78098fee4fd50b7c0c94608
Jonathan Stites <mail@jonstites.com>
2019-02-13 20:26:10 -0600

    Fixes mistake in ci with new directory

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 main() {
-
+    set -ex
     cargo build --target "$TARGET" --release
     
     local tmpdir="$(mktemp -d)"
     local name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
     local staging="${tmpdir}/${name}"
-
+    
+    mkdir -p ${staging}
     cp target/$TARGET/release/fractious $staging/
     cp {README.md, LICENSE-MIT, LICENSE-APACHE} $staging
 
     (cd "$tmpdir" && tar czf "$(pwd)/deployment/${name}.tar.gz" "${name}")
     rm -rf $tmpdir
+    set +ex
 }
 
 main
